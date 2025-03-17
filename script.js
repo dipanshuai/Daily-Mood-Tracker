2f76cee71194888b1980fca2c1dc87ddb101873a
Dipanshu <dipanshu@Dipanshus-Mac-Studio.local>
2025-03-18 02:12:05 +0530

    version 1.1.0: added local storage functionality and retrieval from it

--- script.js
+++ script.js
@@ -2,11 +2,51 @@
 
 let selectedMood = document.getElementById('mood-selector');
 let selectedMoodValue = selectedMood.value;
+
 const submitBtn = document.getElementById('submit-btn');
 const moodCardsContainer = document.getElementById('mood-cards-container');
 const reason = document.getElementById('reason').value;
 
 //++++++++++++ All Functions ++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Local Storage Function------------------------------------------
+
+function saveMood (mood, reason, timestamp){
+   //if there is something in localstorage, get the data else start with an empty array []
+   let moods = JSON.parse(localStorage.getItem('moodEntries')) || [];
+   
+   //add new moodcard data
+   moods.unshift({
+      mood : mood,
+      reason : reason,
+      timestamp: timestamp
+   });
+
+
+   //save updated array to local storage
+
+   localStorage.setItem('moodEntries', JSON.stringify(moods));
+};
+function loadAllMoods(){
+   let moods = JSON.parse(localStorage.getItem('moodEntries')) || [];
+
+   //show each mood entries on the page
+   moods.forEach(moodcard => {
+      let card = document.createElement('div');
+      card.classList.add('innerrow');
+      card.innerHTML= `
+      <div>
+        <h3>Mood: ${moodcard.mood}</h3>
+        <p>Reason: ${moodcard.reason}</p>
+      </div>
+      <p>Time: ${moodcard.timestamp}</p>`;
+
+      moodCardsContainer.appendChild(card);
+   })
+
+}
+
+//local storage functions end ---------------------------------------
 
 function getTimeStamp(){
    const d = new Date();
@@ -21,13 +61,14 @@ function getTimeStamp(){
 function createNewMoodCard (){
    const nowTime = getTimeStamp();
 
-   const reason = document.getElementById('reason');
-   const newMoodCard = document.createElement('div');
+   let reason = document.getElementById('reason');
+   let reasonValue = reason.value;
+   let newMoodCard = document.createElement('div');
    newMoodCard.classList.add('innerrow');
    newMoodCard.innerHTML = `
                 <div>
                     <h3>Mood : ${selectedMoodValue}</h3>
-                    <p>Reason: ${reason.value}</p>
+                    <p>Reason: ${reasonValue}</p>
                 </div>
                 <p>Time:${nowTime}</p>`;
 
@@ -36,12 +77,17 @@ function createNewMoodCard (){
    } else {
       moodCardsContainer.insertBefore(newMoodCard, moodCardsContainer.firstChild);
    }
+
+   //save the new card to local storage
+   saveMood(selectedMoodValue, reasonValue, nowTime)
+
    reason.value = '';
-   selectedMood = '';
+   selectedMood.value = selectedMood.options[0].value;
+
 
 }
 
-console.log(getTimeStamp())
+
 
 //+++++++++++++ All Event Listeners +++++++++++++++++++++++++++++++++++++++++++
 selectedMood.addEventListener('change', function () {
@@ -49,4 +95,6 @@ selectedMood.addEventListener('change', function () {
 })
 
 submitBtn.addEventListener('click', createNewMoodCard)
+
+document.addEventListener('DOMContentLoaded', loadAllMoods);
 
